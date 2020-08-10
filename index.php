171c37750693577a620b5fbb31e3e12d441ee38d
bolderraj <57497532+bolderraj@users.noreply.github.com>
2020-08-10 16:07:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
-
+//this is new file
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
